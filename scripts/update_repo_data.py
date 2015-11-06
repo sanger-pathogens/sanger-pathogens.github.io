731bfdfd23aa1df1848e787461ee9c1ac92508d6
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-11-06 17:38:52 +0000

    Get the org name from config

--- scripts/update_repo_data.py
+++ scripts/update_repo_data.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+import yaml
+from yaml.scanner import ScannerError
+
+class ConfigError(ValueError):
+  pass
+
+def get_orgname():
+  try:
+    with open('config.yml', 'r') as config_file:
+      config = yaml.load(config_file)
+    return config['github_organisation']
+  except FileNotFoundError:
+    raise ConfigError('Please make sure that "config.yml" exists')
+  except ScannerError as e:
+    raise ConfigError("Please make sure that \"config.yml\" is valid Yaml: \n%s" % e)
+  except (TypeError, KeyError):
+    raise ConfigError('Please make sure that "config.yml" contains the key "github_organisation"')
+
 if __name__ == '__main__':
   org_name = get_orgname()
   github_data = get_github_data(org_name)
