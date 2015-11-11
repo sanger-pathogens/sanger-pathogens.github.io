078d505a822a65f92f308dd57e64f6ff7102155f
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-11-11 15:40:20 +0000

    Add bootstrap

--- scripts/update_pages.py
+++ scripts/update_pages.py
@@ -12,6 +12,11 @@ def get_loader():
   logger.debug("Loading templates from '%s'" % templates_dir)
   return FileSystemLoader(templates_dir)
 
+def site_dir():
+  script_dir = os.path.dirname(os.path.realpath(__file__))
+  parent_dir = os.path.dirname(script_dir)
+  return os.path.join(parent_dir, 'site')
+
 logger = logging.getLogger('sanger_pathogens.update_pages')
 
 if __name__ == '__main__':
@@ -19,4 +24,6 @@ if __name__ == '__main__':
                       level=logging.DEBUG)
   env = Environment(loader=get_loader())
   index_template = env.get_template('index.html')
-  print(index_template.render(who='world!'))
+  index_path = os.path.join(site_dir(), 'index.html')
+  with open(index_path, 'w') as index_file:
+    print(index_template.render(who='world!'), file=index_file)
