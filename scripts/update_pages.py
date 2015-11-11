8e3bd04899c7ffc95dc8a4736845ec9700bf6588
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-11-11 17:28:54 +0000

    Add featured repos and human readable time

--- scripts/update_pages.py
+++ scripts/update_pages.py
@@ -35,11 +35,15 @@ if __name__ == '__main__':
   index_path = os.path.join(site_dir(), 'index.html')
   data = get_data()
   repos = data['repos']
+  repos = sorted(repos, key=lambda r: r['score'], reverse=True)
+  featured_repos = repos[:3]
+  other_repos = repos[3:]
   name = data['name']
   organisation_name = data['organisation_name']
   collected_at = data['collected_at']
   with open(index_path, 'w') as index_file:
-    print(index_template.render(repos=repos,
+    print(index_template.render(featured_repos=featured_repos,
+                                other_repos=other_repos,
                                 name=name,
                                 organisation_name=organisation_name,
                                 collected_at=collected_at),
